3f4431925e6b6824c52cd4617fd0e3a3dc3a7dc2
Prosen-Ghosh <prosenghosh25@gmail.com>
2017-08-18 03:03:48 +0600

    video frame added to home page in v2

--- fbExtension/script.js
+++ fbExtension/script.js
@@ -7,3 +7,8 @@ if(_4ejcHeight != null && _4ejcHeight != undefined)_4ejcHeight.style.height = '0
 
 var _4ejc = document.getElementsByClassName("_4ejc");
 if(_4ejc != null && _4ejc != undefined)_4ejc[0].style.display = "none";
+
+var add = document.getElementById('pagelet_ego_pane').style.display = "none";
+document.getElementById('u_0_m').style.display = "none";
+
+$('div._26z1').append("<div><iframe width='320' height='300' src='https://www.youtube.com/embed/HQVRVfBgKUA' frameborder='0' allowfullscreen></iframe></div>");
