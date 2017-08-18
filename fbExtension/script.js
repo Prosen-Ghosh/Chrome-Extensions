3a3bc21f0755caec2bd83cd9b52ca0b924fa4dc8
Prosen-Ghosh <prosenghosh25@gmail.com>
2017-08-18 23:26:24 +0600

    some code issue fixed

--- fbExtension/script.js
+++ fbExtension/script.js
@@ -5,10 +5,14 @@ blueBar[0].style.borderBottom = "1px solid lightseagreen";
 var _4ejcHeight = document.getElementById('pagelet_canvas_nav_content');
 if(_4ejcHeight != null && _4ejcHeight != undefined)_4ejcHeight.style.height = '0px';
 
-var _4ejc = document.getElementsByClassName("_4ejc");
-if(_4ejc != null && _4ejc != undefined)_4ejc[0].style.display = "none";
+// var _4ejc = document.getElementsByClassName("_4ejc");
+// if(_4ejc != null && _4ejc != undefined)_4ejc[0].style.display = "none";
 
-var add = document.getElementById('pagelet_ego_pane').style.display = "none";
-document.getElementById('u_0_m').style.display = "none";
+// var add = document.getElementById('pagelet_ego_pane').style.display = "none";
+// document.getElementById('u_0_m').style.display = "none";
 
-$('div._26z1').append("<div><iframe width='320' height='300' src='https://www.youtube.com/embed/HQVRVfBgKUA' frameborder='0' allowfullscreen></iframe></div>");
+// $('div._26z1').append("<div><iframe width='320' height='300' src='https://www.youtube.com/embed/HQVRVfBgKUA' frameborder='0' allowfullscreen></iframe></div>");
+
+$(document).ready(function(){
+    $('.home_right_column').attr('style','display:none;');
+});
